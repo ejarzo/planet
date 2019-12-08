0d42b3b1ec516f55dbcf72493c962b8594a3f206
ejarzo <ejarz25@gmail.com>
2019-12-08 17:29:17 -0500

    update merch page

--- src/templates/tshirts-page.js
+++ src/templates/tshirts-page.js
@@ -40,12 +40,24 @@ export const TShirtPageTemplate = props => {
                 {title}
               </h2>
               <div className="content">
+                <div
+                  className="columns "
+                  style={{ flexDirection: 'row', marginTop: 30 }}
+                >
+                  {products.slice(0, 2).map(({ description, image }) => (
+                    <div className="column is-6">
+                      <div className="shirt-box">
+                        <PreviewCompatibleImage imageInfo={image} />
+                      </div>
+                    </div>
+                  ))}
+                </div>
                 <MarkdownContent content={description} />
                 <div
                   className="columns is-multiline"
                   style={{ flexDirection: 'row', marginTop: 30 }}
                 >
-                  {products.map(({ description, image }) => (
+                  {products.slice(2).map(({ description, image }) => (
                     <div className="column is-3">
                       <div className="shirt-box">
                         <PreviewCompatibleImage
@@ -125,63 +137,7 @@ export const TShirtPageQuery = graphql`
     markdownRemark(id: { eq: $id }) {
       frontmatter {
         title
-        image {
-          childImageSharp {
-            fluid(maxWidth: 2048, quality: 100) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-        heading
         description
-        intro {
-          blurbs {
-            image {
-              childImageSharp {
-                fluid(maxWidth: 240, quality: 64) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-            text
-          }
-          heading
-          description
-        }
-        main {
-          heading
-          description
-          image1 {
-            alt
-            image {
-              childImageSharp {
-                fluid(maxWidth: 526, quality: 92) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-          }
-          image2 {
-            alt
-            image {
-              childImageSharp {
-                fluid(maxWidth: 526, quality: 92) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-          }
-          image3 {
-            alt
-            image {
-              childImageSharp {
-                fluid(maxWidth: 1075, quality: 72) {
-                  ...GatsbyImageSharpFluid
-                }
-              }
-            }
-          }
-        }
         products {
           description
           image {
@@ -190,23 +146,6 @@ export const TShirtPageQuery = graphql`
                 ...GatsbyImageSharpFluid
               }
             }
-          }
-        }
-        full_image {
-          childImageSharp {
-            fluid(maxWidth: 526, quality: 100) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-        pricing {
-          heading
-          description
-          plans {
-            description
-            items
-            plan
-            price
           }
         }
       }
