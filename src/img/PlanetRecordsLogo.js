b289cfd9e67d2e59503fdd43e864a1f4b0cb087d
ejarzo <ejarz25@gmail.com>
2020-01-08 12:13:12 -0500

    cosmetic updates

--- src/img/PlanetRecordsLogo.js
+++ src/img/PlanetRecordsLogo.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default ({ style, className }) => (
   <svg
     className={className}
-    style={{ fill: 'white', ...style }}
+    style={{ fill: '#ffbd00', ...style }}
     version="1.1"
     id="Layer_1"
     xmlns="http://www.w3.org/2000/svg"
